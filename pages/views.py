350a530988d1a364a5ebe311cd8f65147dcad8f6
sbj276 <sbj276@gmail.com>
2019-10-12 13:13:36 +0530

    Learned about URL redirection , Template basics , Automated tests

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-	return HttpResponse('<h1>Hello, Sachin</h1>')
-# Create your views here.
+class HomePageView(TemplateView):
+	template_name = 'home.html'
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def homePageView(request):
+# 	return HttpResponse('<h1>Hello, Sachin</h1>')
+# # Create your views here.
